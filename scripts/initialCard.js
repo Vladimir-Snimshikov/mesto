41c37ded1e701986ed75c718b1b9edc53d7bbf7c
Vladimir Snimshikov <vladimirsnimshikov@yandex.ru>
2022-10-14 20:24:45 +0300

    refactor:Исправление ошибокпосле Ревью 1

---
+++ scripts/initialCard.js
@@ -0,0 +1,26 @@
+const initialCard = [
+  {
+    "name": "Роза Хутор",
+    "link": "./images/roza-hutor.jpg"
+  },
+  {
+    "name": "Олимпийский парк",
+    "link": "./images/olympic-park.jpg"
+  },
+  {
+    "name": "Краснодар",
+    "link": "./images/krasnodar.jpg"
+  },
+  {
+    "name": "Москва",
+    "link": "./images/moscow.jpg"
+  },
+  {
+    "name": "Санкт-Петербург",
+    "link": "./images/Sankt-petersburg.jpg"
+  },
+  {
+    "name": "Сочи",
+    "link": "./images/sochi.jpg"
+  }
+];
